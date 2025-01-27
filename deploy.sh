eca250d0399f60c07b0379593e3f7e2df63b0e85
Jacob O'Donnell <jacobodonnell@gmail.com>
2025-01-26 19:49:54 -0500

    fix assets script

--- deploy.sh
+++ deploy.sh
@@ -2,9 +2,10 @@ git branch -D deploy
 git co -b deploy
 npm run build
 cp dist/index.html .
-mkdir -p assests
+mkdir -p assets
+cp dist/assets/* assets/.
 git add index.html
-git add assests
+git add assets
 git commit -m "deploy commit"
 git push origin deploy -f
 rm -rf dist
